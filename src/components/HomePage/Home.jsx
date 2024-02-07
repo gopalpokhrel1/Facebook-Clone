f10954f0ab75cbe44dc2745aa69e3d462160217a
Gopal Pokhrel <gopalpokhrel101@gmail.com>
2024-02-07 12:11:57 +0545

    Changes in Home Page

--- src/components/HomePage/Home.jsx
+++ src/components/HomePage/Home.jsx
@@ -20,102 +20,148 @@ import { useState } from 'react';
 
 
 
-// ... (imports remain the same)
-
 export default function Home() {
-    const [displaySections, setDisplaySections] = useState({
-      home: true,
-      page: false,
-      video: false,
-      marketplace: false,
-      groups: false,
-    });
-  
-    const [activeSections, setActiveSections] = useState({
-      home: true,
-      page: false,
-      video: false,
-      marketplace: false,
-      groups: false,
-    });
-  
-    const setActive = (val) => {
-      const updatedDisplaySections = {};
-      const updatedActiveSections = {};
-  
-      Object.keys(displaySections).forEach((section) => {
-        updatedDisplaySections[section] = section === val;
-        updatedActiveSections[section] = section === val;
-      });
-  
-      setDisplaySections(updatedDisplaySections);
-      setActiveSections(updatedActiveSections);
-    };
-  
-    return (
-      <>
-        <section className={styles.main}>
-          <nav className={styles.nav}>
-            <section className={styles.nav_left}>
-              <Image
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/600px-Facebook_Logo_%282019%29.png"
-                width={40}
-                height={40}
-                alt="Photo"
-              />
-              <input type="text" className={styles.nav_input} placeholder="Search facebook" />
-            </section>
-  
-            <section className={styles.nav_middle}>
-              {['home', 'page', 'video', 'marketplace', 'groups'].map((section) => (
-                <section
-                  key={section}
-                  onClick={() => setActive(section)}
-                  className={`${styles[section]} ${displaySections[section] ? styles.display : ''} ${
-                    activeSections[section] ? styles[`blue_${section}`] : styles.icon
-                  }`}
-                >
-                  <Link href="#">
-                    {getIconComponent(section)}
-                  </Link>
-                </section>
-              ))}
-            </section>
-  
-            <section className={styles.nav_right}>
-              <section className={styles.menu}>
-                <CgMenuGridR className={styles.icon2} />
-              </section>
-              <section className={styles.messenger}>
-                <FaFacebookMessenger className={styles.icon2} />
-              </section>
-              <section className={styles.bell}>
-                <FaBell className={styles.icon2} />
-              </section>
-              <section className={styles.photo}>
-                <p className={styles.icon2}>p</p>
-              </section>
-            </section>
-          </nav>
-        </section>
-      </>
-    );
-  }
-  
-  const getIconComponent = (section) => {
-    switch (section) {
-      case 'home':
-        return <AiFillHome />;
-      case 'page':
-        return <PiFlag />;
-      case 'video':
-        return <MdOndemandVideo />;
-      case 'marketplace':
-        return <BsShopWindow />;
-      case 'groups':
-        return <HiUserGroup />;
-      default:
-        return null;
+
+    const [displayhome, setDisplayhome] = useState(true);
+    const [displaypage, setDisplaypage] = useState(false);
+    const [displayvideo, setDisplayvideo] = useState(false);
+    const [displaymarketplace, setDisplaymarketplace] = useState(false);
+    const [displaygroups, setDisplaygroups] = useState(false);
+    const [home, setHome] = useState();
+    const [page, setPage] = useState(false);
+    const [video, setVideo] = useState(false);
+    const [ marketplace, setMarketplace] = useState(false);
+    const [groups, setGroups] = useState(false)
+
+    const setActive=(val)=>{
+       if(val == 'home'){
+        setDisplayhome(true);
+        setHome(true);
+        setDisplaypage(false);
+        setPage(false);
+        setDisplayvideo(false);
+        setVideo(false);
+        setDisplaymarketplace(false);
+        setMarketplace(false);
+        setDisplaygroups(false);
+        setGroups(false);
+       }
+      else if(val == 'page'){
+        setDisplayhome(false);
+        setHome(false);
+        setDisplaypage(true);
+        setPage(true);
+        setDisplayvideo(false);
+        setVideo(false);
+        setDisplaymarketplace(false);
+        setMarketplace(false);
+        setDisplaygroups(false);
+        setGroups(false);
+       }
+      else if(val == 'video'){
+        setDisplayhome(false);
+        setHome(false);
+        setDisplaypage(false);
+        setPage(false);
+        setDisplayvideo(true);
+        setVideo(true);
+        setDisplaymarketplace(false);
+        setMarketplace(false);
+        setDisplaygroups(false);
+        setGroups(false);
+       }
+      else if(val == 'marketplace'){
+        setDisplayhome(false);
+        setHome(false);
+        setDisplaypage(false);
+        setPage(false);
+        setDisplayvideo(false);
+        setVideo(false);
+        setDisplaymarketplace(true);
+        setMarketplace(true);
+        setDisplaygroups(false);
+        setGroups(false);
+       }
+      else if(val == 'groups'){
+        setDisplayhome(false);
+        setHome(false);
+        setDisplaypage(false);
+        setPage(false);
+        setDisplayvideo(false);
+        setVideo(false);
+        setDisplaymarketplace(false);
+        setMarketplace(false);
+        setDisplaygroups(true);
+        setGroups(true);
+       }
+
     }
-  };
+  return (
+    <>
+
+      <section className={styles.main}>
+
+        <nav className={styles.nav}>
+            <section className={styles.nav_left}>
+                <Image src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/600px-Facebook_Logo_%282019%29.png' width={40} height={40} alt='Photo'/>
+                <input type="text" className={styles.nav_input} placeholder='Search facebook' />
+            </section>
+
+            <section className={styles.nav_middle}>
+
+                <section onClick={()=> setActive('home')} className={`${styles.home} ${displayhome ? styles.display: ""}`}>
+                    <Link href="#">
+                       <AiFillHome className={`${home ? styles.blue_home : styles.icon}`}/>
+                    </Link>
+                </section>
+
+                <section onClick={()=> setActive('page')} className={`${styles.page} ${displaypage ? styles.display: ""}`}>
+                    <Link href='#'>
+                        <PiFlag className={`${page ? styles.blue_page : styles.icon}`}/>
+                    </Link>
+                </section>
+
+                <section onClick={()=> setActive('video')} className={`${styles.video} ${displayvideo ? styles.display: ""}`}>
+                    <Link href='#'>
+                    <MdOndemandVideo className={`${video ? styles.blue_video : styles.icon}`}/>
+                    </Link>
+                </section>
+
+                <section onClick={()=> setActive('marketplace')} className={`${styles.marketplace} ${displaymarketplace ? styles.display: ""}`}>
+                    <Link href='#'>
+                    <BsShopWindow className={`${marketplace ? styles.blue_marketplace : styles.icon}`}/>
+                    </Link>
+                </section>
+
+                <section onClick={()=> setActive('groups')} className={`${styles.groups} ${displaygroups ? styles.display: ""}`}>
+                    <Link href='#' >
+                     <HiUserGroup className={`${groups ? styles.blue_groups : styles.icon}`}/>
+                    </Link>
+                </section>
+
+            </section>
+
+            <section className={styles.nav_right}>
+             <section className={styles.menu}>
+                <CgMenuGridR className={styles.icon2}/>
+             </section>
+             <section className={styles.messenger}>
+                <FaFacebookMessenger className={styles.icon2}/>
+             </section>
+
+             <section className={styles.bell}>
+              <FaBell className={styles.icon2}/>
+             </section>
+
+             <section className={styles.photo}>
+                <p className={styles.icon2}>p</p>
+                
+             </section>
+            </section>
+        </nav>
+
+        </section>
       
+    </>
+  )
+}
